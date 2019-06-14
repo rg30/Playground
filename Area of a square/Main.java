7e84fbcca74eaf415e4607984bf2119bd7a98916
rg30 <51487492+rg30@users.noreply.github.com>
2019-06-15 00:25:17 +0530

    Test code pushed to repository.

---
+++ Area of a square/Main.java
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int area, side;
+  
+  side=9 ;
+  area=side*side;
+  printf ("%d sq cm", area ) ;
+  return 0;
+}
---
+++ Multiply two numbers/Main.java
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  int mil;
+  mil=63*81;
+  printf("%d", mil) ;
+  return 0;
+}
